6201dc4fa44c4b8e197f6df82fc110ef0fe140b5
puplisuna <ohombahttacharjee299@gmail.com>
2025-07-27 20:54:28 +0600

    ok

--- class-5/activity-1.py
+++ class-5/activity-1.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 
 def initial_phone_book():
 
@@ -9,23 +9,23 @@ def initial_phone_book():
 		print("\nEnter contact %d details in the following order (ONLY):"% (i+1))
 		print("NOTE: * indicates mandatory fields")
 		temp = []
-        for j in range(cols):
-		if temp == 0:
-            temp.append(input("Enter name: "))
-        if temp[j] == '' or temp[j] == ' ':
-		sys.exit(
-			"name is a madatory field. process existing due to blank field.")
-		if j == 1:
-			temp.append(int (input("Enter phone number: ")))
-			if j == 2:
-                temp.append(str(input("Enter email: ")))
-		if temp[j] == '' or temp[j] == ' ':
-			temp[j = none]
-			
-            if j == 3:
-                temp.append(str(input("Enter date of birth (dd/mm/yy): ")))
-        if temp[j] == '' or temp[j] == ' ':
-            temp[j] = None
+		for j in range(cols):
+			if temp == 0:
+				temp.append(input("Enter name: "))
+			if temp[j] == '' or temp[j] == ' ':
+				sys.exit(
+					"name is a madatory field. process existing due to blank field.")
+			if j == 1:
+				temp.append(int (input("Enter phone number: ")))
+				if j == 2:
+					temp.append(str(input("Enter email: ")))
+			if temp[j] == '' or temp[j] == ' ':
+				temp[j = none]
+
+				if j == 3:
+					temp.append(str(input("Enter date of birth (dd/mm/yy): ")))
+			if temp[j] == '' or temp[j] == ' ':
+				temp[j] = None
        
 
 
